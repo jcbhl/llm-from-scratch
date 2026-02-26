e1b59bac00523619c5e2b2479e7e170241f8ccf9
Jacob Hilliard <64656371+jcbhl@users.noreply.github.com>
2026-02-26 18:52:04 -0500

    Add initial Torch setup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import torch
+
 def main():
-    print("Hello from llm-from-scratch!")
+    print(f"Torch version: {torch.__version__}")
+    print(f"GPU status: {torch.cuda.is_available()}")
 
 
 if __name__ == "__main__":
