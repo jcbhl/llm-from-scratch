719196d5fd7bc7ec978857809bc72c50cd983673
Jacob Hilliard <64656371+jcbhl@users.noreply.github.com>
2026-02-27 21:31:47 -0500

    Add CPU vs GPU matmul runtime comparison

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import matplotlib
 
 matplotlib.use("qtagg")
 
+import time
+
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -42,10 +45,7 @@ class PlaceholderDataset(Dataset):
         return self.labels.shape[0]
 
 
-def main():
-    print(f"Torch version: {torch.__version__}")
-    print(f"GPU status: {torch.cuda.is_available()}")
-
+def train_and_eval():
     x_train = torch.tensor(
         [[-1.2, 3.1], [-0.9, 2.9], [-0.5, 2.6], [2.3, -1.1], [2.7, -1.5]]
     )
@@ -96,6 +96,41 @@ def main():
 
     plt.plot(loss_values)
     plt.show()
+
+
+def matmul(size, device):
+    start = time.time()
+    A = torch.randn(size=(size, size), device=device)
+    B = torch.randn(size=(size, size), device=device)
+
+    A @ B
+    end = time.time()
+    return end - start
+
+
+def main():
+    print(f"Torch version: {torch.__version__}")
+    print(f"GPU status: {torch.cuda.is_available()}")
+
+    sizes = np.logspace(2, 3.8, dtype=np.int64)
+    cpu_times = []
+    gpu_times = []
+    cpu = torch.device("cpu")
+    gpu = torch.device("cuda")
+    for size in sizes:
+        print(size)
+        cpu_time = matmul(size, cpu)
+        gpu_time = matmul(size, gpu)
+        cpu_times.append(cpu_time)
+        gpu_times.append(gpu_time)
+        print(f"CPU time: {cpu_time}\nGPU time: {gpu_time}")
+
+    plt.plot(sizes, cpu_times, label="CPU")
+    plt.plot(sizes, gpu_times, label="GPU")
+    plt.legend()
+    plt.show()
+
+    # train_and_eval()
 
 
 if __name__ == "__main__":
