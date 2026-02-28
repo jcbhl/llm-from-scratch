2d2c4c159fc25fbfb5ad06df46437b804f45522b
Jacob Hilliard <64656371+jcbhl@users.noreply.github.com>
2026-02-27 19:42:29 -0500

    Add initial NN class and setup formatter

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import torch
+import torch.nn as nn
+
+torch.manual_seed(42)
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(num_inputs, 30),
+            nn.ReLU(),
+            torch.nn.Linear(30, 20),
+            nn.ReLU(),
+            torch.nn.Linear(20, num_outputs),
+        )
+
+    def forward(self, input):
+        logits = self.layers(input)
+        return logits
+
 
 def main():
     print(f"Torch version: {torch.__version__}")
     print(f"GPU status: {torch.cuda.is_available()}")
+
+    network = NeuralNetwork(1, 1)
+    print(
+        f"Number of trainable parameters in NN: {sum(p.numel() for p in network.parameters() if p.requires_grad)}"
+    )
+    for parameter in network.parameters():
+        print(parameter.shape)
+    print(network.forward(torch.tensor([0.0])))
 
 
 if __name__ == "__main__":
